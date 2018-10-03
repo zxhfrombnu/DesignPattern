c0e2553774c05201894b822c7826c93cdd663d30
Helen <helen@Alexs-MacBook-Air.local>
2018-10-03 09:25:05 -0400

    use synchronized for threadsafe

---
+++ singleton/threadsafe/src/threadsafe/Singleton.java
@@ -0,0 +1,16 @@
+package threadsafe;
+
+public class Singleton {
+	private static Singleton singleton;
+	private Singleton(){}
+	public static synchronized Singleton getInstance() {
+		if(singleton == null) {
+			singleton = new Singleton();
+		}
+		return singleton;
+	}
+
+	public String getDescription() {
+		return "use the synchronized way";
+	}
+}
