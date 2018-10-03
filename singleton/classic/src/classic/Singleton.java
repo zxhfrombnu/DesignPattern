e87892567e1c244bc93fe285176af20c4a48603f
Helen <helen@Alexs-MacBook-Air.local>
2018-10-03 09:23:36 -0400

    use simple way for singleton

---
+++ singleton/classic/src/classic/Singleton.java
@@ -0,0 +1,15 @@
+package classic;
+
+public class Singleton {
+	private static Singleton instance;
+	private Singleton(){}
+	public static Singleton getInstance() {
+		if(instance == null) {
+			instance = new Singleton();
+		}
+		return instance;
+	}
+	public String getDescription() {
+		return "I'm a classic Singleton . ";
+	}
+}
