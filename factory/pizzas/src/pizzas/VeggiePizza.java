2961827dde002ed98f1735179bb6e0a6fa9e083e
Helen <helen@Alexs-MacBook-Air.local>
2018-10-11 11:19:37 -0400

    simple factory to make pizza

---
+++ factory/pizzas/src/pizzas/VeggiePizza.java
@@ -0,0 +1,11 @@
+package pizzas;
+
+public class VeggiePizza extends Pizza {
+	public VeggiePizza() {
+		name = "Veggie Pizza";
+		dough = "Crust";
+		sauce = "Marinara sauce";
+		toppings.add("Shredded mozzarella");
+		toppings.add("Diced onion");
+	}
+}
