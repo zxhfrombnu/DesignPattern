6de0c4f32b1219d09c91f4488484322f4ef973ba
Helen <helen@Alexs-MacBook-Air.local>
2018-10-11 11:46:19 -0400

    use factory mode to create pizza

---
+++ factory/pizzafm/src/pizzafm/ChicagoStyleCheesePizza.java
@@ -0,0 +1,14 @@
+package pizzafm;
+
+public class ChicagoStyleCheesePizza extends Pizza {
+	public ChicagoStyleCheesePizza() {
+		name = "Chicago Style Deep Dish Cheese Pizza";
+		dough = "Extra Thick Crust Dough";
+		sauce = "Plum Tomato Sauce";
+		toppings.add("Cheese");
+	}
+
+	public void cut() {
+		System.out.println("Cutting the pizza into square slice");
+	}
+}
